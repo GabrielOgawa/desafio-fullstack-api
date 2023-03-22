470d84e8242d127d60e369f0249d0a0ecfc8f2f7
Gabriel <gabriel.ogawa@hotmail.com>
2023-03-22 11:17:13 -0300

    feat: add login

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import "express-async-errors";
 import express from "express";
 import userRoutes from "./routes/users.routes";
 import handleError from "./errors/handleError";
+import sessionRoutes from "./routes/session.routes";
 
 const app = express();
 app.use(express.json());
 
 app.use("/users", userRoutes);
+app.use("/login", sessionRoutes);
 
 app.use(handleError);
 
